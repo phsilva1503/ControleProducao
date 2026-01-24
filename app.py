689e8960f4614a28b5bd8a3fdab3513d7eb99aa0
phsilva1503 <phsilva1@outlook.com.br>
2026-01-24 18:08:16 -0300

    ajuste no app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from models import *
 from flask_migrate import Migrate
@@ -5,16 +6,22 @@ from routes import routes  # Importa suas rotas definidas
 from flask_login import LoginManager
 
 # -----------------------
+# DIRETÓRIO BASE E TEMPLATES
+# -----------------------
+base_dir = os.path.dirname(os.path.abspath(__file__))
+template_dir = os.path.join(base_dir, "templates")
+
+# -----------------------
 # CONFIGURAÇÃO DO APP
 # -----------------------
-app = Flask(__name__)
+app = Flask(__name__, template_folder=template_dir)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///producao.db'  # Banco SQLite
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '123'  # Para produção, gerar uma chave mais segura
 
 # -----------------------
 db.init_app(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 
 login_manager = LoginManager()
 login_manager.login_view = "login"  # rota de login
@@ -33,3 +40,4 @@ routes(app)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
